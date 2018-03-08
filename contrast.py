7ad358ea7c6b34200f609e705e162f80194f6bb1
fangchungao <fangchungao@rltx.com>
2018-03-08 14:55:45 +0800

    1.实现弹框，获取两个文件，退出

--- contrast.py
+++ contrast.py
@@ -1,16 +1,21 @@
 from tkinter import *
 import tkinter.filedialog
 root = Tk()
+list_file = []
 
 def xz():
     filenames = tkinter.filedialog.askopenfilenames()
     print(" you choose file is",filenames)
-    root.destroy()
     if len(filenames) != 0:
-        string_filename =""
-        for i in range(0,len(filenames)):
-            string_filename += str(filenames[i])+"\n"
-        lb.config(text = "您选择的文件是："+string_filename)
+        list_file.append(filenames)
+        if(2 == len(list_file)):
+            print("end ......")
+            root.destroy()
+        else:
+            string_filename =""
+            for i in range(0,len(filenames)):
+                string_filename += str(filenames[i])+"\n"
+            lb.config(text = "您选择的文件是："+string_filename)
     else:
         lb.config(text = "您没有选择任何文件");
 
@@ -19,5 +24,7 @@ lb.pack()
 btn = Button(root,text="弹出选择文件对话框",command=xz)
 btn.pack()
 root.mainloop()
+
+
 
 # link http://blog.csdn.net/Abit_Go/article/details/77938938
