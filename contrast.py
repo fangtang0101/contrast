320485bdd4aaa5445cac7500b205ca820586b08c
fangchungao <fangchungao@rltx.com>
2018-03-08 17:21:31 +0800

    1.判断文件路径是否存在 是否是 xls 文件

--- contrast.py
+++ contrast.py
@@ -1,11 +1,21 @@
 from tkinter import *
 import tkinter.filedialog
+import os
+
 root = Tk()
+root.title('contrast the differences of the two files (you must choose forment is xls)')
+root.geometry('400x200') 
+
 list_file = []
 
 def xz():
     filenames = tkinter.filedialog.askopenfilenames()
     print(" you choose file is",filenames)
+    if not is_xls(str(filenames)):
+        print(" is xls",is_xls(str(filenames)))
+        lb.config(text = "this file is not xls");
+        return
+
     if len(filenames) != 0:
         list_file.append(filenames)
         if(2 == len(list_file)):
@@ -18,6 +28,25 @@ def xz():
             lb.config(text = "您选择的文件是："+string_filename)
     else:
         lb.config(text = "您没有选择任何文件");
+
+# def  is file (xls)
+def is_xls(file):
+    mark = True
+    if os.path.isfile(file):
+        mark = False
+    if not file.endswith('.xls'):
+        mark = False
+    return mark
+    
+
+# contrast two file 
+def contrast_file():
+    pass
+
+
+
+
+# # # start
 
 lb = Label(root,text = '')
 lb.pack()
